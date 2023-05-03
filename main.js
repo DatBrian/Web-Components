e8e8ddba37d76290bde9c7c0d6ab06070fb03dbd
DatBrian <vikard.2005@hotmail.com>
2023-05-03 09:30:01 -0500

    feat: :tada: Se agrega el proyecto al repositorio.

---
+++ main.js
@@ -0,0 +1,5 @@
+import myHeader from "./components/my-header/my-header.js";
+import mySection from "./components/my-section/my-section.js";
+import myNav from "./components/my-nav/my-nav.js";
+import mySelection from "./components/my-selection/my-selection.js";
+import myFooter from "./components/my-footer/my-footer.js";
